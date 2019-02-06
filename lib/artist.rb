1ec489f3bfeb4bae27c78ac0c61952a533986405
Samidh Patel <spatel13456@gmail.com>
2019-02-06 13:05:51 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,8 +1,19 @@
+require 'pry'
+
 class Artist 
   
   attr_accessor :name
   
-  def initialize()
+  def initialize
+    binding.pry
     @name = 'Beyonce'
   end 
 end 
+
+# We can create a class that will associate the name of the artist
+# with the name of the song. 
+# We first initialize the name of the artist, for example (name)
+# We can then create a new object called artist_name = Artist.new
+# ("Beyonce")
+# This will call artist_name.name = "Beyonce"
+# Since we have an object for Song created by an artist by calling song.artist, we can say song_name.artist.name = "Beyonce", because "Beyonce" belongs to the artist and song. If artist.name => Beyonce 
--- lib/song.rb
+++ lib/song.rb
@@ -6,3 +6,11 @@ class Song
     @title = title
   end
 end 
+
+# => "method call"
+# Song would be initialized with a title so song_name = Song.new
+# ("Song_Name")
+# So song.title = "Song_Name"
+# Now we have an artist attribute, so song_name.artist = "Beyonce"
+# So song_name.artist => "Beyonce"
+# But we haven't associated the name of the artist with the song that they have, so we can create a new class called Artist. 
